3bd854da47e87406399141d67a6ad9fcbaf462f1
jiayi_ko <ellenkosasi@gmail.com>
2024-09-10 15:38:56 +0700

    fix minor things in home page

--- src/components/Home/About/About.jsx
+++ src/components/Home/About/About.jsx
@@ -25,37 +25,46 @@ const About = () => {
                 <h1 className="section__title about__title">
                     What is <span className="cp__highlight">Competitive Programming?</span>
                 </h1>
-                <span>
-                Competitive Programming, or CP in short, is a mind sport in which contestants are given a set of programming problems to be solved within a preset amount of time.
-                </span>
-                <br />
-                <span>
-                    Competitions are supported by various companies such as   
-                    {companies.map((company) => (
-                        <a href={company.link}>{company.name}, </a>
-                    ))}
-                    and others.
-                </span>
+                <div className="text-container">
+                    <span>
+                    Competitive Programming, or CP in short, is a mind sport in which contestants are given a set of programming problems to be solved within a preset amount of time.
+                    </span>
+                    <div className="links-container">
+                        Competitions are supported by various companies such as 
+                        {companies.map((company) => (
+                            <span key={company.name}>
+                                <a href={company.link}>{company.name}</a>, 
+                            </span>
+                        ))}
+                        and others.
+                    </div>
+                </div>
             </div>
 
             <div className="jb__section">
                     <h1 className="section__title about__title">
                         What is <span className='jb__highlight'>Jollybee?</span>
                     </h1>
-                    <span>
-                        Jollybee is Bina Nusantara University's official CP team. Currently, Jollybee has 30+ active members being coached directly by the former chief judge of the Indonesian National Science Olympiad in Informatics (OSN bidang Komputer) 2020 and former judge of the International Olympiad in Informatics (IOI) 2022.
-                    </span>
-                    <span>
-                        Jollybee has won numerous national and international competitions and has represented Indonesia in the ICPC World Finals several times.
-                        Please visit our 
-                        {socials.map((social) => (
-                            <span key={social.name}>
-                                <a href={social.link}>{social.name}</a>
-                                , 
-                            </span>
-                                
-                        ))}
-                    </span>
+                    <div className="text-container">
+                        <span>
+                            Jollybee is Bina Nusantara University's official CP team. Currently, Jollybee has 30+ active members being coached directly by the former chief judge of the Indonesian National Science Olympiad in Informatics (OSN bidang Komputer) 2020 and former judge of the International Olympiad in Informatics (IOI) 2022.
+                        </span>
+                        <span>
+                            Jollybee has won numerous national and international competitions and has represented Indonesia in the ICPC World Finals several times.
+                            
+                            <div className="links-container">
+                                Please visit our 
+                                {socials.map((social) => (
+                                    <span key={social.name}>
+                                        <a href={social.link}>{social.name}</a>
+                                        , 
+                                    </span>   
+                                ))}
+                                for further information.
+                            </div>
+                        </span>
+                    </div>
+                    
             </div>
         </div>
     )
