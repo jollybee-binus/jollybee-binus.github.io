51b32cda20a35ef7ee76663121887b9cc17dc4a7
AVM.Martin <avmm.martin+github@gmail.com>
2024-12-04 16:37:50 +0700

    Updates

--- JS/others.js
+++
@@ -1,7 +0,0 @@
-function fixScrollbar() {
-    var isMobile = window.orientation > -1;
-    if (isMobile) {
-        var sbroot = document.querySelector(":root");
-        sbroot.style.setProperty("--pseudo-sb-w", "15px");
-    }
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Jollybee-Recruitment-Website
-
-temporary indefinitely
